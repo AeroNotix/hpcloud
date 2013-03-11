94589aeb9acb40ceb1413c63efbdb86a91fc0562
AeroNotix <aaron.l.france@gmail.com>
2013-03-11 16:00:43 +0100

    Reorganized the URL.go module and added the CDN url.

--- urls.go
+++ urls.go
@@ -1,6 +1,12 @@
 package hpcloud
 
-var OBJECT_STORE = "https://region-b.geo-1.objects.hpcloudsvc.com/v1.0/"
+/* Identity */
 var REGION_URL = "https://region-b.geo-1.identity.hpcloudsvc.com:35357/v2.0/"
 var TOKEN_URL = REGION_URL + "tokens"
-var TENANT_URL = TOKEN_URL + "tenants"
+var TENANT_URL = REGION_URL + "tenants"
+
+/* Object store */
+var OBJECT_STORE = "https://region-b.geo-1.objects.hpcloudsvc.com/v1.0/"
+
+/* CDN */
+var CDN_URL = "https://region-b.geo-1.cdnmgmt.hpcloudsvc.com/v1.0/"
