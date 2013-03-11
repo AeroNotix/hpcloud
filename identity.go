93f9a8a166d2bcacb17cb48e3e668b7d2a9539e3
AeroNotix <aaron.l.france@gmail.com>
2013-03-11 09:51:10 +0100

    Moved Identity related stuff into the identity.go file.

--- identity.go
+++ identity.go
@@ -1,5 +1,159 @@
 package hpcloud
 
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+)
+
+func Authenticate(user, pass, tenantID string) (*Access, error) {
+	l := Login{
+		auth{
+			credentials{
+				User: user, Pass: pass,
+			},
+			tenantID,
+		},
+	}
+	d, err := json.Marshal(l)
+
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+	resp, err := http.Post(
+		REGION_URL+"tokens",
+		"application/json",
+		strings.NewReader(string(d)),
+	)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		return nil, err
+	}
+	a := &Access{}
+	a.TenantID = tenantID
+	switch resp.StatusCode {
+	case http.StatusOK:
+		err = json.Unmarshal(body, a)
+		if err != nil {
+			return nil, err
+		}
+		return a, nil
+	case http.StatusBadRequest:
+		b := BadRequest{}
+		err = json.Unmarshal(body, &b)
+		if err != nil {
+			return nil, err
+		}
+		a.Fail = b
+		return a, nil
+	case http.StatusUnauthorized:
+		u := Unauthorized{}
+		err = json.Unmarshal(body, &u)
+		if err != nil {
+			return nil, err
+		}
+		a.Fail = u
+		return a, nil
+	}
+	panic("Unreachable!")
+}
+
+func (a *Access) GetTenants() {
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", REGION_URL+"tenants", nil)
+	req.Header.Add("X-Auth-Token", a.A.Token.ID)
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer resp.Body.Close()
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	t := &Tenants{}
+	err = json.Unmarshal(b, t)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	a.Tenants = t.T
+}
+
+func (a Access) TenantForName(name string) (string, error) {
+	for _, tenant := range a.Tenants {
+		if tenant.Name == name {
+			return tenant.ID, nil
+		}
+	}
+	return "", errors.New("No tenant ID for the supplied name.")
+}
+
+func (a Access) ScopeToken(name string) (*Access, error) {
+	t := TenantScope{
+		Scope{
+			name,
+			SubToken{
+				ID: a.Token(),
+			},
+		},
+	}
+	b, err := json.Marshal(t)
+	if err != nil {
+		return nil, err
+	}
+	resp, err := http.Post(
+		REGION_URL+"tokens",
+		"application/json",
+		strings.NewReader(string(b)),
+	)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	newa := &Access{}
+	switch resp.StatusCode {
+	case http.StatusOK:
+		err = json.Unmarshal(body, newa)
+		if err != nil {
+			return nil, err
+		}
+		return newa, nil
+	case http.StatusBadRequest:
+		b := BadRequest{}
+		err = json.Unmarshal(body, &b)
+		if err != nil {
+			return nil, err
+		}
+		newa.Fail = b
+		return newa, nil
+	case http.StatusUnauthorized:
+		u := Unauthorized{}
+		err = json.Unmarshal(body, &u)
+		if err != nil {
+			return nil, err
+		}
+		newa.Fail = u
+		return newa, nil
+	}
+	panic("Unreachable!")
+}
+
 type Access struct {
 	A struct {
 		Token    Token            `json:"token"`
