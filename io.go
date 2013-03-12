182ae4465ae8c29b8a121805f3aba2a923647e4f
AeroNotix <aaron.l.france@gmail.com>
2013-03-12 14:40:10 +0100

    Added documentation to the io.go page for the HashedFile type.

--- io.go
+++ io.go
@@ -9,6 +9,11 @@ import (
 	"os"
 )
 
+/*
+  HashedFile is an io.ReadWriter which reads a file into memory whilst
+  giving you access to the hashed contents and only reading the file
+  once
+*/
 type HashedFile struct {
 	MD5          hash.Hash
 	FileContents *bytes.Reader
@@ -16,6 +21,9 @@ type HashedFile struct {
 	Length       int
 }
 
+/*
+  Implements io.Writer
+*/
 func (h *HashedFile) Write(p []byte) (int, error) {
 	i, err := io.WriteString(h.MD5, string(p))
 	if err != nil {
@@ -28,14 +36,24 @@ func (h *HashedFile) Write(p []byte) (int, error) {
 	return len(p), nil
 }
 
+/*
+  Implements io.Reader
+*/
 func (h HashedFile) Read(p []byte) (n int, err error) {
 	return h.FileContents.Read(p)
 }
 
+/*
+  Returns the current hash of the file.
+*/
 func (h HashedFile) Hash() string {
 	return fmt.Sprintf("%x", h.MD5.Sum(nil))
 }
 
+/*
+  Helper function to open, hash and return an io.ReadWriter of the
+  file.
+*/
 func OpenAndHashFile(filename string) (*HashedFile, error) {
 	f, err := os.Open(filename)
 	if err != nil {
