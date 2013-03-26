ff013a17f2a946fc3c3989e55716cf3459b54216
Gorah <elon.aep.delin@gmail.com>
2013-03-26 21:54:31 +0100

    added structs for create instance and CreateInstance method UNTESTED

--- rdb.go
+++ rdb.go
@@ -9,8 +9,11 @@ import (
 	"net/http"
 )
 
+/*
+ ListDBInstances will list all the available database instances
+*/
 func (a Access) ListDBInstances() (*DBInstances, error) {
-	body, err := a.baseRDBRequest("instances", "GET", nil)
+	body, err := a.baseRDBRequest("instances", "GET", nil, nil)
 	if err != nil {
 		return nil, err
 	}
@@ -22,7 +25,34 @@ func (a Access) ListDBInstances() (*DBInstances, error) {
 	return dbs, nil
 }
 
-func (a Access) baseRDBRequest(url, method string, b io.Reader) ([]byte, error) {
+/*
+ CreateDBInstance creates new database instance in the HPCloud using
+settings found in the DatabaseReq instance passed to this function
+
+ This function implements the interface as described in:
+ http://api-docs.hpcloud.com/hpcloud-rdb-mysql/1.0/content/create-instance.html
+*/
+func (a Access) CreateDBInstance (db DatabaseReq) (*NewDBInstance. error){
+	b, err := db.MarshalDBJSON()
+	if err != nil{
+		return nil, err
+	}
+	
+	body, err := a.baseRDBRequest("instances", "POST",
+		strings.NewReader(string(b)), 119)
+	if err != nil{
+		return nil, err
+	}
+
+	sr := &NewDBInstance{}
+	err = json.Unmarshal(body, sr)
+	if err != nil{
+		return nil, err
+	}
+	return sr, nil
+}
+
+func (a Access) baseRDBRequest(url, method string, b io.Reader, conLen int) ([]byte, error) {
 	path := fmt.Sprintf("%s%s/%s", RDB_URL, a.TenantID, url)
 	req, err := http.NewRequest(method, path, b)
 	if err != nil {
@@ -32,6 +62,9 @@ func (a Access) baseRDBRequest(url, method string, b io.Reader) ([]byte, error) 
 	req.Header.Add("Accept", "application/json")
 	req.Header.Add("Content-Type", "application/json")
 	req.Header.Add("X-Auth-Token", a.AuthToken())
+	if conLen != nil{
+		req.Header.Add("Content-Length", conLen)
+	}
 
 	resp, err := a.Client.Do(req)
 	if err != nil {
@@ -46,8 +79,8 @@ func (a Access) baseRDBRequest(url, method string, b io.Reader) ([]byte, error) 
 	switch resp.StatusCode {
 	case http.StatusOK:
 		return body, nil
-	case http.ServerCreated:
-		//add Created Response
+	case http.StatusCreated:
+		return body, nil
 	case http.StatusUnathorized:
 		ua := &Unathorized{}
 		err = json.Unmarshal(body, ua)
@@ -56,9 +89,19 @@ func (a Access) baseRDBRequest(url, method string, b io.Reader) ([]byte, error) 
 		}
 		return nil, errors.New(ua.Message())
 	case http.StatusForbidden:
-		//forbidden err
+		fr := &Forbidden{}
+		err = json.Unmarshal(body, fr)
+		if err != nil {
+			return nil, err
+		}
+		return nil, errors.New(fr.Message())
 	case http.StatusInternalServerError:
-		//int serv err
+		ise := &InternalServerError{}
+		err  = json.Unmarshal(body, ise)
+		if err != nil {
+			return nil, err
+		}
+		return nil, errors.New(ise.Message())
 	case http.StatusNotFound:
 		nf := &NotFound{}
 		err = json.Unmarshal(body, nf)
@@ -88,4 +131,56 @@ type DBInstance struct {
 
 type DBInstances struct {
 	Instances []DBInstance `json:"instances"`
+}
+
+/*
+ This type describes the JSON data which should be sent to the 
+create database instance resource.
+*/
+type DatabaseReq struct{
+	Instance Database `json:"instance"`
+}
+
+type Database struct{
+	Name string `json:"name"`
+	FlavorRef string `json:"flavorRef"`
+	Port int `json:"port"`
+	Dbtype DatabaseType `json:"port"`
+}
+
+type DatabaseType struct{
+	Name string `json:"name"`
+	Version string `json:"version"`
+}
+
+/*
+ This type describes JSON response from a successful CreateDBInstance
+ call.
+*/
+type NewDBInstance struct{
+	Created string `json:"created"`
+	Credential DBCredentials `json:"credential"`
+	Flavor Flavor_ `json:"flavor"`
+	Hostname string `json:"hostname"`
+	Id string `json:"id"`
+	Links []link `json:"links"`
+	Name string `json:"name"`
+	SecurityGroups []DBSecGroups `json:"security_groups"`
+	Status string `json:"status"`
+}
+
+/*
+ This type describes Database Security groups 
+*/
+type DBSecGroups struct{
+	Id string `json:"id"`
+	Links []Link `json:"links"`
+}
+
+/*
+ This type describes Database Credentials
+*/
+type DBCredentials struct{
+	Password string `json:"password"`
+	Username string `json:"username"`
 }
